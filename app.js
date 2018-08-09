3ef338f2556e2d116444a9ee94be4f6a8a1f9b53
Mohammad Abdullah Saed <mohdsaed@hungerstation.com>
2018-08-09 04:56:48 +0300

    Create public folder and install frontend dependencies

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express  = require('express');
 const http     = require('http');
 const socketIO = require('socket.io');
+const path     = require('path');
 
 const app    = express();
 const server = http.createServer(app);
 const io     = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 io.on('connection', socket => {
   console.log('client connected');
